ce931c8be1aaf115febefd017c26a1fb73043b26
Xinhao Yuan <xinhaoyuan@gmail.com>
2019-07-13 08:30:38 -0400

    put icon into the theme

--- init.lua
+++ init.lua
@@ -3,6 +3,16 @@ local editor = require(... .. ".editor")
 local switcher = require(... .. ".switcher")
 local default_editor = editor.create()
 local default_layout = layout.create("default", default_editor)
+local gcolor = require("gears.color")
+
+local beautiful = require("beautiful")
+local source = debug.getinfo(1, "S").source
+if source:sub(1, 1) == "@" then
+   base = source:match("^@(.-)[^/]+$")
+   beautiful.layout_machi = gcolor.recolor_image(
+      base .. "icon.png", beautiful.fg_normal)
+   print(beautiful.layout_machi)
+end
 
 return {
    layout = layout,
